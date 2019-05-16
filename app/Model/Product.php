19ead2e80dd1f1518c02f7f9912ff84d0b7268c8
Mohosin <mohosin16103308@gmail.com>
2019-05-16 07:39:39 +0600

    Adding Relationship and check in Tinker

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -3,8 +3,14 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use app\Model\Review;
 
 class Product extends Model
 {
-    //
+    public function reviews()
+    {
+        
+        return $this->hasMany('app/Model/Review'); 
+        
+    }
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -2,9 +2,15 @@
 
 namespace App\Model;
 
-use Illuminate\Database\Eloquent\Model;
 
+use Illuminate\Database\Eloquent\Model;
+use app\Model\Product;
 class Review extends Model
 {
-    //
+    public function product()
+    {
+        
+        return $this->belongsTo('app\Model\Product'); 
+        
+    }
 }
